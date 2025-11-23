5e19bec08591293e286320567e58e98dd3a425b4
Alexoga9 <alexalga32@gmail.com>
2025-11-22 20:03:26 -0400

    Squashed commit of the following:
    commit fdf8fea0cc7bade172a07a6f3f0d579116582427
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Sat Nov 22 17:55:03 2025 -0500
    
        rr
    
    commit cd598224dc1091132c318820cb1311d4911b0934
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Sat Nov 22 17:03:53 2025 -0500
    
        deberia estar todo ahi
    
    commit 13f7030c44601c7f97a6ca47144e0f0ced2556a3
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Fri Nov 21 19:31:16 2025 -0500
    
        ll
    
    commit 671c17b284164f84ee8d30c1411058ee30c58c54
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Fri Nov 21 19:27:21 2025 -0500
    
        Add zasteroids.dae tracked by LFS
    
    commit e8c719aac3b580d1719e249c493f6829b959c280
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Fri Nov 21 19:13:21 2025 -0500
    
        tracking files with LFS
    
    commit a21ee3e55417395eb9fd404b0e599eae81e0d468
    Author: Jussetv2 <jussetvidal27@gmail.com>
    Date:   Mon Nov 3 22:31:53 2025 -0500
    
        primera actividad

---
+++ Assets/Pract-Jusse/scripts/activahumo.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class activahumo : MonoBehaviour
+{
+    public GameObject objeto; 
+
+    private bool activado = false;
+
+    private void Start()
+    {
+        if (objeto != null)
+            objeto.SetActive(false); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activado && other.CompareTag("Player_prueba"))
+        {
+            if (objeto != null)
+                objeto.SetActive(true);
+
+            activado = true; 
+        }
+    }
+}
+
